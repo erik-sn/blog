a14bf53b94e0faea512933964668d48fbc7e2307
Erik Niehaus <nieh.erik@gmail.com>
2017-04-17 17:18:20 -0400

    removed extra css call

--- config/server.production.tsx
+++ config/server.production.tsx
@@ -119,7 +119,6 @@ function renderFullPage(html: string, version: string, initialStore: Store<any>)
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <link rel="icon" href="https://res.cloudinary.com/dvr87tqip/image/upload/v1492291999/sandbox_xb9z2t.png" type="image/x-icon" />
         <link rel="stylesheet" href="/static/bundle.min.${version}.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.10.0/styles/solarized-light.min.css">
       </head>
       <body id="app-body">
         <div id="app-container">${html}</div>
