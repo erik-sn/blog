1ae45629ee9a64a4745da6d9ab76e485902e7ae7
Erik Niehaus <nieh.erik@gmail.com>
2017-04-22 07:18:36 -0400

    added social media links to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,16 @@ import * as React from 'react';
 const Footer = () => (
   <footer className="footer__container" >
     <p>&#9400; Copyright 2017 Erik Niehaus</p>
+    <div className="social_sidebar__link">
+      <a href="https://github.com/erik-sn/">
+        <img height="30px" src="https://res.cloudinary.com/dvr87tqip/image/upload/v1492859750/githubwhite_yfwonv.png" />
+      </a>
+    </div>
+    <div className="social_sidebar__link">
+      <a href="https://twitter.com/en_says/">
+        <img height="30px" src="https://res.cloudinary.com/dvr87tqip/image/upload/v1492859750/twitter_white_n4netl.png" />
+      </a>
+    </div>
   </footer>
 );
 
