6e1ef863c7fa398b092f1e1845eb5c66ffe06ebf
kiresuah <kiresuah@gmail.com>
2017-05-04 13:14:49 -0400

    added viewport meta tag to html

--- config/server.production.tsx
+++ config/server.production.tsx
@@ -115,8 +115,9 @@ function renderFullPage(html: string, version: string, initialStore: Store<any>)
   return `
     <!doctype html>
     <html>
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
       <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Dev Sandbox</title>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.10.0/styles/solarized-light.min.css">
