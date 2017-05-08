c5c90c662cae7364fbec68dd161b7c6adfb79fc3
Erik Niehaus <nieh.erik@gmail.com>
2017-05-07 23:41:17 -0400

    fixed bug in connected router

--- src/connected_router.tsx
+++ src/connected_router.tsx
@@ -8,6 +8,16 @@ export const LOCATION_CHANGE = '@@router/LOCATION_CHANGE';
 
 class ConnectedRouter extends Component<any, any> {
 
+  static propTypes = {
+    store: PropTypes.object,
+    history: PropTypes.object,
+    children: PropTypes.node,
+  };
+
+  static contextTypes = {
+    store: PropTypes.object,
+  };
+
   public componentWillMount(): void {
     const { children } = this.props;
 
@@ -24,7 +34,6 @@ class ConnectedRouter extends Component<any, any> {
       <Router {...props} history={history}>
         <Route render={({ location }: any) => {
             return <MountedRoute store={store} location={location} children={children} />;
-          // tslint:disable-next-line:jsx-alignment
           }}/>
       </Router>
     );
